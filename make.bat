4291e0e7cda672763dc4fa158e7da264b9064bab
fakekey2k <fakekey2k@Windows>
2025-05-10 20:58:00 +0700

    fix path

--- make.bat
+++ make.bat
@@ -4,10 +4,10 @@ cd build
 
 cmake ^
     -GNinja ^
-    -H"C:/Users/nghilq/Desktop/Chokevy" ^
-    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="C:/Users/nghilq/Desktop/Chokevy/build" ^
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="C:/Users/nghilq/Desktop/Chokevy/build" ^
-    -B"C:/Users/nghilq/Desktop/Chokevy/build" ^
+    -H"C:/Users/fakekey2k/Desktop/Chokevy" ^
+    -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="C:/Users/fakekey2k/Desktop/Chokevy/build" ^
+    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="C:/Users/fakekey2k/Desktop/Chokevy/build" ^
+    -B"C:/Users/fakekey2k/Desktop/Chokevy/build" ^
     ..
 
 cmake --build .
